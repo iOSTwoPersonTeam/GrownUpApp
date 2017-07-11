9470dc70b9ff3c0175747bf894ab417c65f2f42f
mac <mac@MAC.local>
2017-07-11 09:55:03 +0800

    Initial Commit

---
+++ 成长之路APPTests/____APPTests.m
@@ -0,0 +1,39 @@
+//
+//  ____APPTests.m
+//  成长之路APPTests
+//
+//  Created by mac on 2017/7/11.
+//  Copyright © 2017年 hui. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ____APPTests : XCTestCase
+
+@end
+
+@implementation ____APPTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
