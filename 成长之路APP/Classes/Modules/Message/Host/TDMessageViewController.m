b27f624a50c30ba0bea96a6d21b8b3780517ef85
chengdenghui <1017096321@qq.com>
2017-09-12 11:43:32 +0800

    1

--- 成长之路APP/Classes/Modules/Message/Host/TDMessageViewController.m
+++ 成长之路APP/Classes/Modules/Message/Host/TDMessageViewController.m
@@ -7,9 +7,14 @@
 //
 
 #import "TDMessageViewController.h"
-#import "TDMessageDetailViewController.h"
+#import "ZJScrollPageView.h"
+#import "TDPersonMessageViewController.h"
+#import "TDSystemMessageViewController.h"
 
-@interface TDMessageViewController ()
+@interface TDMessageViewController ()<ZJScrollPageViewDelegate>
+@property(nonatomic,strong)ZJScrollPageView *scrollPageView;
+@property(nonatomic,strong)NSArray *themeArray; //主题分类
+@property(nonatomic,strong)NSArray *childViewControllers; //子控制器
 
 @end
 
@@ -20,30 +25,90 @@
     // Do any additional setup after loading the view.
     
     self.view.backgroundColor =[UIColor redColor];
-    
+    [self.view addSubview:self.scrollPageView]; //添加标题segment
 }
 
--(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+#pragma mark ---private---
+
+
+
+
+
+#pragma mark ----Delagate---
+#pragma mark ---ZJScrollPageViewDelegate
+-(NSInteger)numberOfChildViewControllers
 {
-    TDMessageDetailViewController *VC =[[TDMessageDetailViewController alloc] init];
-    VC.title =@"消息详情";
-    [self.navigationController pushViewController:VC animated:YES];
+    return self.childViewControllers.count;
+}
+
+-(UIViewController<ZJScrollPageViewChildVcDelegate> *)childViewController:(UIViewController<ZJScrollPageViewChildVcDelegate> *)reuseViewController forIndex:(NSInteger)index
+{
+    UIViewController<ZJScrollPageViewChildVcDelegate> *childVc = (UIViewController<ZJScrollPageViewChildVcDelegate>*)reuseViewController;
+    
+    if (!childVc) {
+        childVc = self.childViewControllers[index];
+    }
+    
+    return childVc;
     
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (BOOL)shouldAutomaticallyForwardAppearanceMethods
+{
+    return NO;
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+
+
+
+#pragma mark --getter---
+-(ZJScrollPageView *)scrollPageView
+{
+    if (!_scrollPageView) {
+        
+        ZJSegmentStyle *style =[[ZJSegmentStyle alloc] init];
+        //显示滚动条
+        style.showLine =YES;
+        //标题不滚动
+        style.scrollTitle =NO;
+        //segmentView是否有弹性
+        style.segmentViewBounces =NO;
+        //取消点击时候的动画效果
+        style.animatedContentViewWhenTitleClicked =NO;
+        //取消内容视图弹框
+        //        style.scrollContentView =NO;
+        //标题颜色
+        style.selectedTitleColor = [UIColor redColor];
+        //标题滚动条颜色
+        style.scrollLineColor = [UIColor redColor];
+        //标题字体
+        style.titleFont =[UIFont systemFontOfSize:16];
+        //颜色渐变
+        style.gradualChangeTitleColor =YES;
+        //初始化
+        _scrollPageView =[[ZJScrollPageView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT) segmentStyle:style titles:self.themeArray parentViewController:self delegate:self];
+        
+    }
+    return _scrollPageView;
 }
-*/
+
+-(NSArray *)themeArray
+{
+    if (!_themeArray) {
+        _themeArray =@[@"系统消息",@"个人消息"];
+    }
+    return _themeArray;
+}
+-(NSArray *)childViewControllers
+{
+    if (!_childViewControllers) {
+        _childViewControllers =@[[[TDSystemMessageViewController alloc]init],
+                                 [[TDPersonMessageViewController alloc]init]];
+    }
+    return _childViewControllers;
+}
+
+
 
 @end
---
+++ 成长之路APP/Classes/Modules/Message/SystemMessage/Controller/TDSystemMessageViewController.h
@@ -0,0 +1,14 @@
+//
+//  TDSystemMessageViewController.h
+//  成长之路APP
+//
+//  Created by mac on 2017/9/12.
+//  Copyright © 2017年 hui. All rights reserved.
+//
+
+#import "TDRootViewController.h"
+#import "ZJScrollPageView.h"
+
+@interface TDSystemMessageViewController : TDRootViewController<ZJScrollPageViewChildVcDelegate>
+
+@end
