ae3c9e7f0c96908169f0b90c450f03c4e5aed23d
chengdenghui <1017096321@qq.com>
2017-10-10 21:20:26 +0800

    解决微信支付问题  sharesdk停留在指定版本 保证这个版本的微信sdk包含支付功能

--- 成长之路APP/Classes/Modules/Profile/RunningTrack/Controller/TDRunningTrackViewController.m
+++ 成长之路APP/Classes/Modules/Profile/RunningTrack/Controller/TDRunningTrackViewController.m
@@ -350,6 +350,7 @@
         _backButton.layer.masksToBounds =YES;
         _backButton.layer.cornerRadius =CGRectGetHeight(_backButton.frame)/2;
         [_backButton addTarget:self action:@selector(clickBack) forControlEvents:UIControlEventTouchUpInside];
+        
     }
     return _backButton;
 }
