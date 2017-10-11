f7e27871b79751b86d45efb8db8fe36cd19422f3
chengdenghui <1017096321@qq.com>
2017-10-11 17:46:58 +0800

    创建easeChatManager

---
+++ 成长之路APP/Classes/Manager/EaseChatManager/TDEaseChatManager.m
@@ -0,0 +1,13 @@
+//
+//  TDEaseChatManager.m
+//  成长之路APP
+//
+//  Created by mac on 2017/10/11.
+//  Copyright © 2017年 hui. All rights reserved.
+//
+
+#import "TDEaseChatManager.h"
+
+@implementation TDEaseChatManager
+
+@end
